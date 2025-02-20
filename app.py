d6e5a8dba948624c4259f82858f0d13ac743c3e7
abcdef364 <abcdesfg821@gmail.com>
2025-02-20 09:44:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ def saludo():
     with open("mensaje.txt", "w") as f:
         f.write("Este es un mensaje guardado en un archivo.")
 
+def suma(a, b):
+    return a + b
+
 saludo()
-"def suma(a, b):`n    return a + b" 
